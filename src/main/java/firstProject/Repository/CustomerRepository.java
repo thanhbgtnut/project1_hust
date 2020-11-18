776c977688f4a9c485d1611097ba48201ece1c0f
thanh-local <thanhbg.tnut@gmail.com>
2020-11-18 11:13:43 +0700

    thanh add crud

--- src/main/java/firstProject/Repository/CustomerRepository.java
+++ src/main/java/firstProject/Repository/CustomerRepository.java
@@ -1,10 +1,17 @@
 package firstProject.Repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import firstProject.entity.Account;
 
 @Repository
 public interface CustomerRepository extends JpaRepository<Account, Long> {
+	
+	@Query(value="select a from Account a where a.id = ?1 and a.status = 1")
+	Optional<Account> getAllAccountActive(Long id);
+	
 }
